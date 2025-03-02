d59bd7b5c80e729c496f079121b803cab7aa6cc9
=Tushar <=tushar09821.gmail.com>
2025-03-02 11:36:36 +0530

    created groupEntity

--- src/main/java/com/splitwise/Splitwise/groups/controller/GroupsController.java
+++ src/main/java/com/splitwise/Splitwise/groups/controller/GroupsController.java
@@ -2,6 +2,8 @@ package com.splitwise.Splitwise.groups.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,10 @@ public class GroupsController {
     public ResponseEntity<?> createGroup(){
         return ResponseEntity.ok().build();
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateGroup(@PathVariable int id){
+     return ResponseEntity.status(301).build();
+    }
+
 }
