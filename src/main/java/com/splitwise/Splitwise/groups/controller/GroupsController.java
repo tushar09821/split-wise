e258e3645e89d63ee1493c6a8d9c20d9b545d26d
=Tushar <=tushar09821.gmail.com>
2025-03-02 11:05:23 +0530

    groups controller setup

---
+++ src/main/java/com/splitwise/Splitwise/groups/controller/GroupsController.java
@@ -0,0 +1,14 @@
+package com.splitwise.Splitwise.groups.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/group")
+public class GroupsController {
+    public ResponseEntity<?> createGroup(){
+        return ResponseEntity.ok().build()git;
+    }
+}
